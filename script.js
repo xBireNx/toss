cefee68cb2ef3bb9b7b2ce1bce1d78d775bd4d7c
Shashwat Biren <shashwat.biren007@gmail.com>
2022-08-23 06:37:50 +0000

    Tweaks

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const buttonClick = () => {
   let randomNum = Math.floor(Math.random() * 2)
   if (randomNum === 1) {
     document.getElementById("ans").src = "https://i.ibb.co/BPhL6xZ/Untitled-design-1.png";
+    document.getElementById("result").textContent = "Result is : Head"
   } else if (randomNum === 0) {
     document.getElementById("ans").src = "https://i.ibb.co/gwBb0Dt/Untitled-design.png";
+    document.getElementById("result").textContent = "Result is : Tail"
   }
 }
